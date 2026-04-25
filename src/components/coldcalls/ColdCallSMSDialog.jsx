61f04584d53b432063eaa6f5f3150b80aab4c97f
dbaile02 <dbaile02@icloud.com>
2026-04-25 19:33:14 +0000

    File changes

--- src/components/coldcalls/ColdCallSMSDialog.jsx
+++ src/components/coldcalls/ColdCallSMSDialog.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
